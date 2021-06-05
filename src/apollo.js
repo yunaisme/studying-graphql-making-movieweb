2295190e9cded1f383a457e2dee37ea4bf3d3bba
yuna <mapleyuna3262@gmail.com>
2021-06-05 15:56:59 +0900

    init

---
+++ src/apollo.js
@@ -0,0 +1,22 @@
+import ApolloClient from "apollo-boost";
+
+const client = new ApolloClient({ 
+  uri: "https://movieql2.vercel.app/",
+  resolvers: {
+    Movie: {
+      isLiked: () => false
+    },
+    Mutation: {
+      toggleLikeMovie: (_, { id, isLiked }, { cache }) => {
+        cache.writeData({
+          id: `Movie:${id}`,
+          data: {
+            isLiked: !isLiked
+          }
+        });
+      }
+    }
+  }
+});
+
+export default client;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import { ApolloProvider } from "@apollo/react-hooks";
+import client from "./apollo";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </ApolloProvider>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
